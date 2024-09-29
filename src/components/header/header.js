0a9434cb724aa258d677fbccbc950502d2acd1f6
MsPritee <badgujarpriti5@gmail.com>
2024-09-29 17:33:55 +0530

    updated header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,8 +1,34 @@
 import React from "react";
 import "./header.css";
+import Logo from "../../assets/images/logo.png";
+import SearchIcon from "@mui/icons-material/Search";
 
-const header = () => {
-  return <div>header</div>;
+
+const Header = () => {
+  return (
+    <>
+      <header>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-sm-2">
+              <img src={Logo} />
+            </div>
+            {/* header search start here */}
+            <div className="col-sm-5">
+              <div className="headerSearch d-flex align-items-center">
+                <div className="selectDrop cursor">All Categories</div>
+                <div className="search">
+                  <input type="text" placeholder="Search for items..." />
+                  <SearchIcon />
+                </div>
+              </div>
+            </div>
+            {/* header search end here */}
+          </div>
+        </div>
+      </header>
+    </>
+  );
 };
 
-export default header;
+export default Header;

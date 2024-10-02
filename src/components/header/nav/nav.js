ae31f816521cedc7d07526897893cd30db1a137b
MsPritee <badgujarpriti5@gmail.com>
2024-10-02 14:29:42 +0530

    updated nav

--- src/components/header/nav/nav.js
+++ src/components/header/nav/nav.js
@@ -4,6 +4,7 @@ import Button from "@mui/material/Button";
 import KeyboardArrowDown from "@mui/icons-material/KeyboardArrowDown";
 import GridView from "@mui/icons-material/GridView";
 import { Link } from "react-router-dom";
+import { HeadphonesOutlined } from "@mui/icons-material";
 
 const Nav = () => {
   return (
@@ -57,6 +58,65 @@ const Nav = () => {
                       Pages <KeyboardArrowDown />
                     </Link>
                   </Button>
+                  <div className="dropdown-menu">
+                    <ul>
+                      <li>
+                        <Button>
+                          <Link to="/about">About Us</Link>
+                        </Button>
+                      </li>
+                      <li>
+                        <Button>
+                          <Link to="/about">Contact</Link>
+                        </Button>
+                      </li>
+                      <li>
+                        <Button>
+                          <Link to="/about">My Account</Link>
+                        </Button>
+                      </li>
+                      <li>
+                        <Button>
+                          <Link to="/about">Login</Link>
+                        </Button>
+                      </li>
+                      <li>
+                        <Button>
+                          <Link to="/about">Register</Link>
+                        </Button>
+                      </li>
+                      <li>
+                        <Button>
+                          <Link to="/about">Forget Password</Link>
+                        </Button>
+                      </li>
+                      <li>
+                        <Button>
+                          <Link to="/about">Reset Password</Link>
+                        </Button>
+                      </li>
+                      <li>
+                        <Button>
+                          <Link to="/about">Purchase Guide</Link>
+                        </Button>
+                      </li>
+                      <li>
+                        <Button>
+                          <Link to="/about">Privacy Policy</Link>
+                        </Button>
+                      </li>
+                      <li>
+                        <Button>
+                          <Link to="/about">Terms of Services</Link>
+                        </Button>
+                      </li>
+                      <li>
+                        <Button>
+                          <Link to="/about">404 Page</Link>
+                        </Button>
+                      </li>
+                    </ul>
+                  </div>
                 </li>
                 <li className="list-inline-item">
                   <Button>
@@ -67,7 +127,17 @@ const Nav = () => {
             </nav>
           </div>
 
-          <div className="col-sm-2 part3 d-flex align-items-center"></div>
+          <div className="col-sm-2 part3 d-flex align-items-center">
+            <div className="phNo d-flex align-items-center ml-auto">
+              <span>
+                <HeadphonesOutlined />
+              </span>
+              <div className="info ml-3">
+                <h3 className=" text-g mb-0">1900-888</h3>
+                <p className="mb-0">24/7 Support Center</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>

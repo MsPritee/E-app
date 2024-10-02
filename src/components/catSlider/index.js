d64b0a96f9348f3e7ab346d53fde960a2de80633
MsPritee <badgujarpriti5@gmail.com>
2024-10-02 18:59:03 +0530

    slider updated

---
+++ src/components/catSlider/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const CatSlider = () => {
+  return (
+    <div className="catSliderSection">
+      <div className="container-fluid"></div>
+    </div>
+  );
+};
+export default CatSlider;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 import HomeSlider from "./slider/index";
+import CatSlider from "../../components/catSlider";
 
 const Home = () => {
-  return <HomeSlider />;
+  return (
+    <>
+      <HomeSlider /> <CatSlider />
+    </>
+  );
 };
 export default Home;

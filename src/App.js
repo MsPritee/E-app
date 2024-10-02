40d7eeeebc4662dbe1df313cdfe4002e342008a8
MsPritee <badgujarpriti5@gmail.com>
2024-10-02 13:35:07 +0530

    added nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Header from "./components/header/header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/index";
+import About from "./pages/About";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-    </>
+      <Routes>
+        <Route exact={true} path="/" element={<Home />} />
+        <Route exact={true} path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/header/nav/nav.js
+++ src/components/header/nav/nav.js
@@ -1,23 +1,73 @@
 import React from "react";
 import "./nav.css";
 import Button from "@mui/material/Button";
+import KeyboardArrowDown from "@mui/icons-material/KeyboardArrowDown";
+import GridView from "@mui/icons-material/GridView";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
-  return;
+  return (
+    <div className="nav d-flex align-items-center">
+      <div className="container-fluid ">
+        <div className="row">
+          <div className="col-sm-3 part1">
+            <Button className="bg-g text-white catTab">
+              <GridView /> &nbsp; Browse All Categories <KeyboardArrowDown />
+            </Button>
+          </div>
 
-  <div className="nav d-flex">
-    <div className="container-fluid align-items-center">
-      <div className="row">
-        <div className="col-sm-3 part1">
-          <Button>Browse All Categories</Button>
+          <div className="col-sm-7 part2">
+            <nav>
+              <ul className="list list-inline mb-0">
+                <li className="list-inline-item">
+                  <Button>
+                    <Link>Home</Link>
+                  </Button>
+                </li>
+                <li className="list-inline-item">
+                  <Button>
+                    <Link>About</Link>
+                  </Button>
+                </li>
+                <li className="list-inline-item">
+                  <Button>
+                    <Link>Shop</Link>
+                  </Button>
+                </li>
+                <li className="list-inline-item">
+                  <Button>
+                    <Link>Vendors</Link>
+                  </Button>
+                </li>
+                <li className="list-inline-item">
+                  <Button>
+                    <Link>Mega Menu</Link>
+                  </Button>
+                </li>
+                <li className="list-inline-item">
+                  <Button>
+                    <Link>Blog</Link>
+                  </Button>
+                </li>
+                <li className="list-inline-item">
+                  <Button>
+                    <Link>Pages</Link>
+                  </Button>
+                </li>
+                <li className="list-inline-item">
+                  <Button>
+                    <Link>Contact</Link>
+                  </Button>
+                </li>
+              </ul>
+            </nav>
+          </div>
+
+          <div className="col-sm-2 part3"></div>
         </div>
-
-        <div className="col-sm-7 part2"></div>
-
-        <div className="col-sm-2 part3"></div>
       </div>
     </div>
-  </div>;
+  );
 };
 
 export default Nav;

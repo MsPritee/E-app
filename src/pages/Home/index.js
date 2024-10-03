2fc9f6fe2d5d2f19549c100c6ea07e2b71f90f3b
MsPritee <badgujarpriti5@gmail.com>
2024-10-03 18:06:54 +0530

    updated home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import HomeSlider from "./slider/index";
 import CatSlider from "../../components/catSlider";
 import Banners from "../../components/banners";
+import "./style.css";
 
 const Home = () => {
   return (
@@ -10,6 +11,31 @@ const Home = () => {
 
       <CatSlider />
       <Banners />
+
+      <section className="homeProducts">
+        <div className="container-fluid">
+          <div className="d-flex align-items-center">
+            <h2 className="hd mb-0 mt-0">Popular Product</h2>
+            <ul className="list list-inline ml-auto filterTab">
+              <li className="list-inline-item">
+                <a className="cursor">All</a>
+              </li>
+              <li className="list-inline-item">
+                <a className="cursor">Dairy Product</a>
+              </li>
+              <li className="list-inline-item">
+                <a className="cursor">Fruits</a>
+              </li>
+              <li className="list-inline-item">
+                <a className="cursor">Dry Fruits</a>
+              </li>
+              <li className="list-inline-item">
+                <a className="cursor">Grocery</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
